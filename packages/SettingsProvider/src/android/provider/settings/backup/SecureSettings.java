594629f7637da6f204dc9df726fb9747919a0480
jhonboy121 <alfredmathew05@gmail.com>
2021-12-17 11:09:37 +0530

    base: allow toggling torch by double/long pressing power button
    * This is acheived by extending existing gestures for camera / assistant, power menu open is not delayed.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -269,5 +269,7 @@ public class SecureSettings {
         Settings.Secure.CAMERA_EXTENSIONS_FALLBACK,
         Settings.Secure.VISUAL_QUERY_ACCESSIBILITY_DETECTION_ENABLED,
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
+        Settings.Secure.TORCH_DOUBLE_TAP_POWER_GESTURE_ENABLED,
+        Settings.Secure.TORCH_LONG_PRESS_POWER
     };
 }

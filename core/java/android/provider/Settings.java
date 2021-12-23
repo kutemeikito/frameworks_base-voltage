2fcf17b37e985f44fc27088206fa282704784611
Ido Ben-Hur <idoybh2@gmail.com>
2021-12-23 17:24:28 +0200

    base: Add a toggle for secure tiles on keyguard [1/2]
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13403,6 +13403,13 @@ public final class Settings {
          */
         public static final String VPN_ENFORCE_DNS_STORE = "vpn_enforce_dns_store";
 
+	/**
+         * Whether to show QS auto brightness toggle button
+         * @hide
+         */
+        @Readable
+        public static final String QSTILE_REQUIRES_UNLOCKING = "qstile_requires_unlocking";
+
         /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.

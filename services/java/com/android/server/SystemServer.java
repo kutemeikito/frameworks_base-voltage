cc93b39674eec1e0582c6490263bb5b7dece5682
LibXZR <i@xzr.moe>
2022-08-20 15:12:23 +0800

    base: Add support for parallel space
    This adds initial support for running multiple instances of the same
    app at the same time. Basically this is done by making use of multiuser.
    
    Further work is needed to make it perfect.
    
    Squashed:
    * base: Expose parallel space owner to api
    * base: Add support for managing files across parallel spaces
    * services: camera: Whitelist parallel space users
    * services: location: Whitelist parallel space users
    * SystemUI: Fix privacy indicator not showing for apps in parallel spaces
    * ParallelSpaceManagerService: Do not install setupwizard too
    * ActivityStarter: Resolve launcher intent to parallel owner user
    * pm: Restore the default behavior of `pm install`
    * fwb: Rename parallel space permission
    * StorageManager: Return pass through mount mode only when parallel users available
    * SystemUserInfoHelper: Fix crash when switching user
    * ParallelSpaceManager: Add method to get the default cloned apps list
    
    Dhina17: Adapted for Android 14
    
    Change-Id: Ib385753df5778a25facd39166abd8b1e29ed7e91
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: electimon <electimon@gmail.com>
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -241,6 +241,7 @@ import com.android.server.webkit.WebViewUpdateService;
 import com.android.server.wm.ActivityTaskManagerService;
 import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
+import com.android.server.voltage.ParallelSpaceManagerService;
 
 import dalvik.system.VMRuntime;
 
@@ -441,6 +442,9 @@ public final class SystemServer implements Dumpable {
             "com.android.server.compat.overrides.AppCompatOverridesService$Lifecycle";
     private static final String HEALTHCONNECT_MANAGER_SERVICE_CLASS =
             "com.android.server.healthconnect.HealthConnectManagerService";
+    private static final String PARALLEL_SPACE_SERVICE_CLASS =
+            "com.android.server.voltage.ParallelSpaceManagerService";
+
     private static final String ROLE_SERVICE_CLASS = "com.android.role.RoleService";
     private static final String GAME_MANAGER_SERVICE_CLASS =
             "com.android.server.app.GameManagerService$Lifecycle";
@@ -3035,6 +3039,10 @@ public final class SystemServer implements Dumpable {
 
         t.traceBegin("HealthConnectManagerService");
         mSystemServiceManager.startService(HEALTHCONNECT_MANAGER_SERVICE_CLASS);
+        t.traceEnd();
+
+        t.traceBegin("StartParallelSpaceManagerService");
+        mSystemServiceManager.startService(PARALLEL_SPACE_SERVICE_CLASS);
         t.traceEnd();
 
         if (mPackageManager.hasSystemFeature(PackageManager.FEATURE_DEVICE_LOCK)) {

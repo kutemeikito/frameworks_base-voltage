NAAP-again/frameworks_base

base: Allow turning on WiFi/Bluetooth when pressing the tiles [1/2]
Respectively
Could save some taps for some users

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -169,5 +169,7 @@ public class SystemSettings {
         Settings.System.LOCKSCREEN_WEATHER_PROVIDER,
         Settings.System.LOCKSCREEN_WEATHER_LOCATION,
         Settings.System.LOCKSCREEN_WEATHER_TEXT,
+        Settings.System.QS_WIFI_AUTO_ON,
+        Settings.System.QS_BT_AUTO_ON,
     };
 }

kutemeikito/frameworks_base-voltage

Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -116,7 +116,9 @@ public class SystemSettings {
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
                 Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
                 Settings.System.LOCKSCREEN_ROTATION,
-                Settings.System.NAVIGATION_BAR_IME_SPACE
+                Settings.System.NAVIGATION_BAR_IME_SPACE,
+	        Settings.System.QS_WIFI_AUTO_ON,
+	        Settings.System.QS_BT_AUTO_ON
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);
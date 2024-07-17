9a7549c41491813ac16b3253f08578610e32f1e5
Dmitrii <bankersenator@gmail.com>
2024-07-17 01:03:37 +0000

    fwb: disable auto reboot by default
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/ext/settings/ExtSettings.java
+++ core/java/android/ext/settings/ExtSettings.java
@@ -35,8 +35,7 @@ public class ExtSettings {
 
     public static final IntSetting AUTO_REBOOT_TIMEOUT = new IntSetting(
             Setting.Scope.GLOBAL, "settings_reboot_after_timeout",
-            // default value: 18 hours
-            (int) TimeUnit.HOURS.toMillis(18));
+            (int) TimeUnit.HOURS.toMillis(0));
 
     public static final BoolSetting SCREENSHOT_TIMESTAMP_EXIF = new BoolSetting(
             Setting.Scope.PER_USER, "screenshot_timestamp_exif", false);
